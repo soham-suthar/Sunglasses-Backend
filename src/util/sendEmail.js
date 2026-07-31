e2e36938a799f37e3b3ccc069001f752f8e49369
soham-suthar <sohamnishithsutharrr@gmail.com>
2026-07-31 21:26:07 +0530

    fix: update email transporter configuration to use Gmail service

--- src/util/sendEmail.js
+++ src/util/sendEmail.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
